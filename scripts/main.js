035a32549132279283efe8e0172729a30ea7c473
Luciano Fernandes <lucianofvital@gmail.com>
2025-04-28 14:55:19 -0300

    Primeiro commit - Projeto Teleprag

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+// Suavizar o Scroll ao clicar nos links do menu
+document.querySelectorAll("a.nav-link").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  });
+});
